cc8d23aa3eb7643525012793b626f625acd12cc5
ruxsara <ruxsarahebibova@Gmail.com>
2024-05-11 20:40:58 +0400

    Feat: create channel details component

---
+++ components/channel-details/channel-details.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export const ChannelDetails = () => {
+  return (
+    <View>
+      <Text>ChannelDetails</Text>
+    </View>
+  );
+};
